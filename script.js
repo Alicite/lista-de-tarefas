069e4a0494af5bb48ca420632f3d69efb66fb303
Alicite <lunacarter1503@gmail.com>
2026-04-07 21:23:24 -0300

    COMITÃO BOMBAAAAA

--- script.js
+++ script.js
@@ -9,27 +9,39 @@ const verificarBD = () => {
 
 }
 
+const limparLista = () => {
+    while (listaDeTarefas.firstChild){
+        listaDeTarefas.removeChild(listaDeTarefas.lastChild)
+    }
+}
+
+const atualizarLista = () => {
+    localStorage.setItem('tarefasSalvas', JSON.stringify(tarefasAtuais))
+    limparLista()
+    tarefasAtuais.forEach((tarefa, i) => criarTarefa(tarefa, i))
+}
+
 const criarTarefa = (texto, indice) => {
     const listaDeTarefas = document.querySelector('#lista-de-tarefas')
     const tarefa = document.createElement('li')
     
     tarefa.classList.add('tarefa')
-    tarefa.dataset.chave = indice
-    tarefa.innerHTML = `${texto} <input type="button" value="❌" class="bt">`
+    tarefa.innerHTML = `${texto} <input type="button" value="❌" class="bt" data-chave="${indice}">`
     listaDeTarefas.appendChild(tarefa)
 };
 
 
 const addTarefa = (texto) => {
     tarefasAtuais.push(texto)
-    localStorage.setItem('tarefasSalvas', JSON.stringify(tarefasAtuais))    
+    localStorage.setItem('tarefasSalvas', JSON.stringify(tarefasAtuais))
 }
 
-const tarefasAtuais = verificarBD();
-tarefasAtuais.forEach((tarefa, i) => criarTarefa(tarefa, i))
+let tarefasAtuais = verificarBD();
 
 const btEnviar = document.querySelector('#bt-enviar')
 const inputTarefa = document.querySelector('#input-tarefa')
+const listaDeTarefas = document.querySelector('#lista-de-tarefas')
+const botaoLimpar = document.querySelector('#bt-limpar')
 
 btEnviar.addEventListener('click', () => {
     const textoTarefa = document.querySelector('#input-tarefa').value
@@ -37,8 +49,8 @@ btEnviar.addEventListener('click', () => {
     if (textoTarefa === ''){
         alert('Insira uma tarefa antes de adicionar!')
     } else {
-        criarTarefa(textoTarefa, tarefasAtuais.length)
         addTarefa(textoTarefa)
+        atualizarLista()
     }
 })
 
@@ -50,8 +62,26 @@ inputTarefa.addEventListener('keyup', (evento) => {
         if (textoTarefa === ''){
             alert('Insira uma tarefa antes de adicionar!')
         } else {
-            criarTarefa(textoTarefa, tarefasAtuais.length)
             addTarefa(textoTarefa)
+            atualizarLista()
         }
     }
 })
+
+listaDeTarefas.addEventListener('click', (evento) => {
+    const alvoDoClique = evento.target
+
+    if (alvoDoClique.type === 'button'){
+        const chave = alvoDoClique.dataset.chave
+        tarefasAtuais.splice(chave, 1)
+        
+        atualizarLista()
+    }
+})
+
+botaoLimpar.addEventListener('click', () => {
+    tarefasAtuais = [];
+    atualizarLista()
+})
+
+atualizarLista()
